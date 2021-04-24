2ff9f835669f09a3c11b813d6fa2ab293497fbc1
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-04-24 13:40:29 -0300

    chore: create users migration

---
+++ src/database/migrations/1619281877462-CreateUsers.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUsers1619281877462 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          { name: 'ID', type: 'uuid', isPrimary: true },
+          {
+            name: 'email',
+            type: 'varchat',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}

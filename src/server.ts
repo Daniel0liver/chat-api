403258e02819d883147fbf5a3684eca999b3937d
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-04-26 22:35:57 -0300

    chore: add socket.io

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
+
 import { routes } from 'routes';
 import 'database';
 
 const app = express();
+
+const http = createServer(app); // Creating http protocol
+const io = new Server(http); // Creating ws protocol
 
 app.use(express.json());
 app.use(routes);
